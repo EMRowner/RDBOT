12b47c86a9fba411bb3ad25f38d838a253945770
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-12 21:15:19 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,16 +1,17 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-	
+  message.channel.send(":x: Bu Komutu Sadece Yapımcım Kullanabilir!");
+  if (message.author.id !== "352722884197744640") return //Buraya ID ni yaz
     message.channel.send(`Bot yeniden başlatılıyor...`).then(msg => {
     console.log(`BOT: Bot yeniden başlatılıyor...`);
     process.exit(0);
-  })
-};
+        })
+  };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
+  guildOnly: true,
   aliases: [],
   permLevel: 4
 };
