9d06f5d53f3740c23fc6e6ccc3fc558d7fd7d6c8
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-16 17:39:04 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,4 @@
 module.exports = member => {
     let username = member.user.username;
-    member.send('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
 };
 
