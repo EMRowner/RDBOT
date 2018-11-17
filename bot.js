e014faa2cf329981fc760ac35a3686e71ac3cd52
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-17 17:32:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,6 +83,21 @@ client.on('message', msg => {
     msg.reply('**Aleyküm selam, Hoşgeldin**');
   }
 });
+client.on("message", msg => {
+        const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq","@mk"];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+             if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+
+                  return msg.channel.send(`${message.author.username} Küfür etti, küfürü engellendi.`).then(msg => msg.delete(3000));
+             }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    });
+
 const Jimp = require('jimp');
 client.on('message', message => {
 if (message.content === `<@${client.user.id}>`) {
