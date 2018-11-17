21383fdb6af24364b428a55c63a12c44d75e54d2
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-17 21:32:38 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,8 +10,8 @@ exports.run = (client, message, params) => {
   .setThumbnail("https://cdn.discordapp.com/attachments/502832520467972116/513046607013609477/BOT_cn.png")
   .setDescription(`[Bot Davet Linki](  https://discordapp.com/oauth2/authorize?client_id=506371036963667979&scope=bot&permissions=2146958847  ) | ` + `[Destek Sunucusu](  https://discord.gg/ByU6xAM  ) \n\n**Ping:** ${Math.round(client.ping)}ms!`)
   .setColor("#FFB900")
-  .addField("**●İD● BOT • Komutlar**", `▫️ **|  id+eğlence:** Eğlence komutlarını gösterir. \n▫️ **|   id+moderasyon:** Moderasyon komutlarını gösterir. \n▫️ **|  id+anakomut:** Temel komutları gösterir. \n▫️ **|  id+resimefekt:** Resim Efektleri komutlarını gösterir.`)
-  .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
+  .addField("**●İD● BOT • Komutlar**", `▫️ **|  id+eğlence:** Eğlence komutlarını gösterir. \n▫️ **|   id+moderasyon:** Moderasyon komutlarını gösterir. \n▫️ **|  id+anakomut:** Temel komutları gösterir. \n▫️ **|  id+resimefekt:** Resim Efektleri komutlarını gösterir \n▫️ **| id+atatürk-yardım:** Atatürk komutlarını gösterir.`)
+  .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL
 
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
