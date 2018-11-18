e1c26d4f5bc493619ad8afd694c3ef59458d009b
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-18 18:32:50 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,2 +1,8 @@
 client.on("guildMemberAdd", member => {
+    let otorol = JSON.parse(fs.readFileSync("./sunucuyaözelayarlar/otorol.json", "utf8"));
+  (function (exports, require, module, __filename, __dirname) { client.on("guildMemberAdd", member => {
+    var role = otorol[member.guild.id].role;
+  const rol = member.guild.roles.find('name', role);
+    if (!rol)
+    member.addRole(role);
 });
