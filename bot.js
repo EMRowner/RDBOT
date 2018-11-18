d6bae2692a71f76658b8b14c980083ab3f57e3c5
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-18 08:22:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,6 +83,23 @@ client.on('message', msg => {
     msg.reply('**Aleyküm selam, Hoşgeldin**');
   }
 });
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(prefix) !== 0) return;
+  const args = message.content.slice(ayarlar.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+    if (command === "espri") {
+    
+    var request = require('request');
+    request('https://api.eggsybot.xyz/espri', function (error, response, body) {
+    if (error) return console.log('Hata:', error); // Hata olursa, konsola göndersin,
+    else if (!error) { // Eğer hata yoksa;
+        var info = JSON.parse(body); // info değişkeninin içerisine JSON'ı ayrıştırsın,
+        message.channel.send('**Espri**: ' + info.soz); // ve konsola çıktıyı versin.
+    }
+});    
+}
+}); 
 client.on("message", msg => {
         const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq","@mk"];
         if (kufur.some(word => msg.content.includes(word))) {
