1debf2a95264909bb9845098e53f2d6f16b7ec1b
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-19 21:18:40 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, params) => {
   .setThumbnail("https://cdn.discordapp.com/attachments/502832520467972116/513046607013609477/BOT_cn.png")
   .setDescription(`[Bot Davet Linki](  https://discordapp.com/oauth2/authorize?client_id=506371036963667979&scope=bot&permissions=2146958847  ) | ` + `[Destek Sunucusu](  https://discord.gg/ByU6xAM  ) \n\n**Ping:** ${Math.round(client.ping)}ms!`)
   .setColor("#FFB900")
-  .addField("**●İD● BOT • Komutlar**", `▫️ **|  id+eğlence:** Eğlence komutlarını gösterir. \n▫️ **|   id+moderasyon:** Moderasyon komutlarını gösterir. \n▫️ **|  id+anakomut:** Temel komutları gösterir. \n▫️ **|  id+resimefekt:** Resim Efektleri komutlarını gösterir \n▫️ **| id+atatürk-yardım:** Atatürk komutlarını gösterir \n▫️ **| id+gelen-gidenyardım** Gelen-giden i yapmayı gösterir.`)
+  .addField("**●ER● BOT • Komutlar**", `▫️ **|  r!eğlence:** Eğlence komutlarını gösterir. \n▫️ **|   r!moderasyon**Moderasyon komutlarını gösterir. \n▫️ **|  r!anakomut**Temel komutları gösterir. \n▫️ **|  r!resimefekt** Resim efekt komutlarını gösterir \n▫️ **| r!atatürk-yardım** Atatürk komutlarını gösterir \n▫️ **| r!gelen-gidenyardım Resimli gelen-giden yapmayı gösterir.
   .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
 
   if (!params[0]) {
