6c408bf3a8228e40a8feec1f4338790335d0cd3f
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-20 16:37:57 +0300

    Create duyuru.js

---
+++ komutlar/duyuru.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args, member) => {
+    let mesaj = args.slice(0).join(' ');
+    if (mesaj.length < 1) return message.channel.send(':x: Duyuruya yazmak için bir şeyler yazınız.');
+  let incidentchannel = message.guild.channels.find(`name`, "duyurular");
+    if(!incidentchannel) return message.channel.send(":x: **duyurular** kanalını bulamıyorum.");
+
+    incidentchannel.send(`@loxy | **Duyuru**\n\n- ${mesaj}`);
+  message.channel.send(':white_check_mark: Duyuru başarıyla yapıldı.')
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['duyuryap', 'duyur'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'duyuru',
+  description: 'Duyuru',
+  usage: 'duyuru <mesaj>'
+};
