8786ef827f2d8225555c8d8b779cd1d71ef44b58
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-11-20 16:50:11 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -7,7 +7,7 @@ exports.run = (client, message) => {
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription('')
-    .addField("» Linkler", `[Bot Davet Linki](  https://discordapp.com/oauth2/authorize?client_id=506371036963667979&scope=bot&permissions=2146958847  )` + "**\n**"+`[Destek Sunucusu](  https://discord.gg/2CMjard  )` + "**\n**"+`[Discord Bots Linki(Onay Bekliyor)](  https://discordbots.org/bot/506371036963667979  )` + "**\n**"+`[Botumuzun Sitesi](  https://alierdem200670.wixsite.com/idbot  )`, false)
+    .addField("» Linkler", `[Bot Davet Linki](  https://discordapp.com/oauth2/authorize?client_id=506371036963667979&scope=bot&permissions=2146958847  )` + "**\n**"+`[Destek Sunucusu](  https://discord.gg/9M4v4yR  )` + "**\n**"+`[Discord Bots Linki(Onay Bekliyor)](  https://discordbots.org/bot/506371036963667979  )` + "**\n**"+`[Botumuzun Sitesi](  https://alierdem200670.wixsite.com/idbot  )`, false)
     .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL);
     return message.channel.sendEmbed(pingozel)
 };
