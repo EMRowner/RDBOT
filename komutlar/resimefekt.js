dac137b87c972ba90154a095eb40db5f5daf54f9
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-02 22:00:35 +0300

    Update resimefekt.js

--- komutlar/resimefekt.js
+++ komutlar/resimefekt.js
@@ -7,7 +7,7 @@ exports.run = (client, message, params) => {
 
   const embedeğlence2 = new Discord.RichEmbed()
   .setColor("#FFB900")
-  .addField("●ER● BOT • Resimefekt Komutları", `▫️ **|  r!wasted:** Profilinize wasted efekti ekler \n▫️ **| r!sniper **Profilinize sniper efekti ekler \n▫️ **| r!kralol **Kralolma GIF i gönderir**  .`)
+  .addField("ƦƛƓЄ ƖMƤƛƦƛƬƠƦԼƲğƲ BOT • Resimefekt Komutları", `▫️ **|  r!wasted:** Profilinize wasted efekti ekler \n▫️ **| r!sniper **Profilinize sniper efekti ekler \n▫️ **| r!kralol **Kralolma GIF i gönderir**  .`)
   .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
 
   if (!params[0]) {
