e00b5031cd38eefc7615d7e591c5db5c50d79bb7
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-03 22:06:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,57 @@ client.on('message', msg => {
     msg.reply('Aleyküm Selam.');
   }
 });
+const snekfetch = require('snekfetch');
+let points = JSON.parse(fs.readFileSync('./xp.json', 'utf8'));
 
+var f = [];
+function factorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+};
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+client.on("message", async message => {
+    if (message.channel.type === "dm") return;
+
+  if (message.author.bot) return;
+
+  var user = message.mentions.users.first() || message.author;
+  if (!message.guild) user = message.author;
+
+  if (!points[user.id]) points[user.id] = {
+    points: 0,
+    level: 0,
+  };
+
+  let userData = points[user.id];
+  userData.points++;
+
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (curLevel > userData.level) {
+    userData.level = curLevel;
+        var user = message.mentions.users.first() || message.author;
+message.channel.send(`:up: **| ${user.username}   Geveze Seni! Gene Level Atladın.*`)
+    }
+
+fs.writeFile('./xp.json', JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+
+  if (message.content.toLowerCase() === prefix + 'profil' || message.content.toLowerCase() === prefix + 'profile') {
+const level = new Discord.RichEmbed().setTitle(`${user.username}`).setDescription(`**Seviye:** ${userData.level}\n**EP (Error Puan):** ${userData.points}`).setColor("RANDOM").setFooter(``).setThumbnail(user.avatarURL)
+message.channel.send(`:pencil: **| ${user.username} Adlı Kullanıcının Profili Burada!**`)
+message.channel.send(level)
+  }
+});
 client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(prefix) !== 0) return;
