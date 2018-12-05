18e5e0e9f7f5cdbebada48e140dda96b23bbcd20
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-05 16:51:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,7 +255,14 @@ client.on("guildMemberAdd", async member => {
   }
  
 });
-
+client.on("guildMemberAdd", member => {
+    let otorol = JSON.parse(fs.readFileSync("./sunucuyaözelayarlar/otorol.json", "utf8"));
+  
+    var role = otorol[member.guild.id].role;
+  const rol = member.guild.roles.find('name', role);
+    if (!rol)
+    member.addRole(role);
+});
  
 
 var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
