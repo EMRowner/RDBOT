c3d8c79a557bf88d32cea7de3959fcc281df2d2d
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-05 16:38:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -257,7 +257,7 @@ client.on("guildMemberAdd", async member => {
 });
 client.on("message", msg => {
   
- fetch(`kufur_${msg.guild.id}`).then(i => {
+(`kufur_${msg.guild.id}`).then(i => {
     if (i == 'acik') {
         const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "am", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq",];
         if (kufur.some(word => msg.content.includes(word))) {
