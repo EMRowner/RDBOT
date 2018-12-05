6ee44bd03ff34db05757d924c6d51cff1b4c94b8
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-05 17:06:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -258,7 +258,7 @@ client.on("guildMemberAdd", async member => {
 client.on("guildMemberAdd", member => {
     let otorol = JSON.parse(fs.readFileSync("./sunucuyaözelayarlar/otorol.json", "utf8"));
   
-    var role = otorol[member.guild.id].role;
+    var role = otorol[member.guild.id];
   const rol = member.guild.roles.find('name', role);
     if (!rol)
     member.addRole(role);
