719ef1116f42489004611738f8b2df27c0a8aa6c
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-08 14:07:35 +0300

    Update otorol.js

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,40 +1,43 @@
 const fs = require("fs");
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
-exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Bu Komutu Kullanmak İçin `Rolleri Yönet` Yetkisine sahip olmalısın.");
-    let otorol = JSON.parse(fs.readFileSync("./sunucuyaözelayarlar/otorol.json", "utf8"));
-    if (!args[0]) { 
-        otorol[message.guild.id] = {
-            role: 0
-        };
-        fs.writeFile("./sunucuyaözelayarlar/otorol.json", JSON.stringify(otorol), (err) => {
-            if (err) console.log(err);
-        });
-        message.reply("Lütfen bir rol ismi yazın.");
-   }
-    if (args[0]) { 
-        let roles = args.join(" ");
-        let role = message.guild.roles.find("name", roles);
-        otorol[message.guild.id] = {
-            role: role.id
-        };
-        fs.writeFile("./sunucuyaözelayarlar/otorol.json", JSON.stringify(otorol), (err) => {
-            if (err) console.log(err)
-        });
-        message.reply(`Otorol Ayarlandı: **${role.name}**`);
-    }
+exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(new Discord.RichEmbed().setColor("RANDOM").setAuthor(message.guild.name, message.guild.iconURL).setDescription(":no_entry_sign: **Bu Komutu Kullanmak İçin __Rolleri Yönet__ Yetkisine Sahip Olmalısın!**"));
+    if (!args[0]) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setAuthor(message.guild.name, message.guild.iconURL).addField('Kullanım;', `:white_check_mark: **eb+otorol <@rol>**`).setFooter('Error Bot', bot.user.avatarURL).setTimestamp());
+	let otorol = JSON.parse(fs.readFileSync("./ayarlar/otorol.json", "utf8"));
+	if (!args[0]) {
+		otorol[message.guild.id] = {
+			role: 0,
+			roless: 0
+		};
+		fs.writeFile("./ayarlar/otorol.json", JSON.stringify(otorol), (err) => {
+			if (err) console.log(err);
+		});
+		message.channel.send(`**${message.author.username}** otorol kapatıldı!`);
+	}
+	if (args[0]) {
+		let roles = message.mentions.roles.first();
+		let role = message.guild.roles.find(r => r.name === roles);
+		otorol[message.guild.id] = {
+			role: roles.id,
+			roless: roles.name
+		};
+		fs.writeFile("./ayarlar/otorol.json", JSON.stringify(otorol), (err) => {
+			if (err) console.log(err)
+		});
+		message.channel.send(`**${message.author.username}** Otorol ayarlandı Rol: **${roles}**`);
+	}
 }
 
 exports.conf = {
- enabled: true,
- guildOnly: false,
- aliases: ['otorol', 'oto-rol', 'oto-rol-ayarla'],
- permLevel: 0
+  enabled: true,
+  guildOnly: true,
+  aliases: ['oto-rol'],
+  permLevel: 2
 };
 
 exports.help = {
- name: 'Otorol',
- description: 'Otorol',
- usage: 'otorol'
-};                                                        
+  name: 'otorol',
+  description: 'otorol.',
+  usage: 'otorol'
+};
