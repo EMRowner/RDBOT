bc8471bdd85915920803303fc07cf355ecb84acc
EMRowner <44610750+EMRowner@users.noreply.github.com>
2018-12-08 10:02:01 +0300

    Update özlüsöz.js

--- komutlar/özlüsöz.js
+++ komutlar/özlüsöz.js
@@ -1,19 +1,18 @@
 const Discord = require('discord.js');
 
  exports.run = (client, message, args) => {
-   var cevaplar = ['**Zihin paraşüt gibidir açık değilse işe yaramaz!**','**Hayat bir bisiklete binmek gibidir. Dengenizi korumak için hareket etmeye devam etmelisiniz.**','**Bütün kurallara uyursanız tüm eğlenceyi kaçırırsınız.**'**Dalgaları durduramazsın, ama sörf yapmayı öğrenebilirsin.**' ];
+   var cevaplar = ['Dalgaları durduramazsın, ama sörf yapmayı öğrenebilirsin.','Zihin paraşüt gibidir. Açık değilse işe yaramaz.','Hayat bir bisiklete binmek gibidir. Dengenizi korumak için hareket etmeye devam etmelisiniz.' ];
    var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
    message.reply(cevap);
 };
 exports.conf = {
  enabled: true,
  guildOnly: false,
- aliases: [''],
+ aliases: ['çaypişir', 'caykaynat','çaykaynat'],
  permLevel: 0 ,
 };
 
 exports.help = {
- name: 'özlüsöz',
- description: '',
- usage: 'özlüsöz'
-};
+ name: 'çay',
+ description: 'çay gifi gönderir',
+ usage: 'cilgin'
